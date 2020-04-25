a8cc91c86ca152aded046e4b55aaec86b6000fd8
macrusal <macrusal@gmail.com>
2020-04-25 17:44:15 -0300

    BDD - Usando Mockito

--- src/test/java/br/com/udemy/springboot/tdd/bdd/MockitoTests.java
+++ src/test/java/br/com/udemy/springboot/tdd/bdd/MockitoTests.java
@@ -2,7 +2,10 @@ package br.com.udemy.springboot.tdd.bdd;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,11 +14,15 @@ import java.util.List;
  * @author macrusal on 25/04/20
  * @project spring-boot-tdd
  */
+@RunWith(MockitoJUnitRunner.class)
 public class MockitoTests {
+
+    @Mock
+    List<String> lista;
+
 
     @Test
     public void primeiroTesteMockito() throws Exception {
-        List<String> lista = Mockito.mock(ArrayList.class);
 
         Mockito.when(lista.size()).thenReturn(20);
 
